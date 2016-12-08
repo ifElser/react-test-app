f71f870dae0f438799552d5a540da8bd716ff622
Evgeny Agafonov <js-coder@mail.ru>
2016-12-08 04:50:27 +0300

    header navigation

--- development/source/components/main-layout/main-layout.jsx
+++ development/source/components/main-layout/main-layout.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Link } from 'react-router'
 
 // import Routes from './routes'
 
@@ -12,12 +13,13 @@ export default class MainLayout extends Component {
 	constructor(props) { super(props); }
 
 	render(){
-		console.log(this.props)
+		console.log(css)
 		let children =  this.props.children instanceof Array ? this.props.children : [this.props.children];
 		return (
 		    <div>
 		    	<header>
-		    		header
+		    		<Link to='/' className={css.headerLink} activeClassName={css.active}>Posts</Link>
+		    		<Link to='/albums' className={css.headerLink} activeClassName={css.active}>Albums</Link>
 		    	</header>
 			    <div className={css.mainLayout}>
 					{this.props.main}
